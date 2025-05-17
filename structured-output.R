9beea4eec04ae31eecb1c72cbe9779d1e941b57e
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-05-18 01:52:05 +1000

    slides up to initial live demo

---
+++ structured-output.R
@@ -0,0 +1,22 @@
+text <- readLines(system.file("examples/third-party-testing.txt", package = "ellmer"))
+# url <- "https://www.anthropic.com/news/third-party-testing"
+# html <- rvest::read_html(url)
+# text <- rvest::html_text2(rvest::html_element(html, "article"))
+
+type_summary <- type_object(
+  "Summary of the article.",
+  author = type_string("Name of the article author"),
+  topics = type_array(
+    'Array of topics, e.g. ["tech", "politics"]. Should be as specific as possible, and can overlap.',
+    type_string(),
+  ),
+  summary = type_string("Summary of the article. One or two paragraphs max"),
+  coherence = type_integer("Coherence of the article's key points, 0-100 (inclusive)"),
+  persuasion = type_number("Article's persuasion score, 0.0-1.0 (inclusive)")
+)
+
+chat <- chat_openai()
+data <- chat$extract_data(text, type = type_summary)
+cat(data$summary)
+
+str(data)
